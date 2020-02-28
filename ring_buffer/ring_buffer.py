652339e261548924c1bcc21be200953da9e53054
Greg <gsc229@gmail.com>
2020-02-28 11:51:46 -0500

    ring_b, psuedo code and initial setup

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -6,9 +6,33 @@ class RingBuffer:
         self.capacity = capacity
         self.current = None
         self.storage = DoublyLinkedList()
+     # test creates a RingBuffer instance with a capacity of 5
 
     def append(self, item):
-        pass
+        # append to the tail up to the capacity
+        # keep the current as the tail until capacity is reached
+        # a app(b) --> a, b etc. current = b
+        # a, b, c, d, e at capacity, add 'f'
+        # after current travels the length of the list once, that means the head will be the oldest. 'f' needs to replace the oldest, which is the head
+        # set current to f
+        # f, b, c, d, e --> add 'g'
+        # now f is the current, which meas the oldest is to the right of it 'b'
+        # replace b with 'g', set 'g' to current
+        # f, g, c, d, e --> add 'h', repeat steps until the current reaches the capacity.
+
+        if self.storage.length < self.capacity:
+            self.storage.add_to_tail(item)
+            self.current = self.storage.tail
+            print(f"\nitem: {item}, current: {self.current.value}")
+
+        elif self.storage.length == self.capacity:
+            print(f"AT CAPACITY...")
+            print(f"CURRENT: {self.current.value}")
+            if not self.current.next:
+                print("**NO CURRENT.NEXT**")
+
+            else:
+                print("**THERE'S A CURRENT.NEXT**")
 
     def get(self):
         # Note:  This is the only [] allowed
@@ -30,3 +54,14 @@ class ArrayRingBuffer:
 
     def get(self):
         pass
+
+
+buffer = RingBuffer(5)
+
+buffer.append('a')
+buffer.append('b')
+buffer.append('c')
+buffer.append('d')
+buffer.append('e')
+
+buffer.append('f')
